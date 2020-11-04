9ac64edc30c062dfbbe6a231fdd9ede12c6078b9
Ta-hiro <erosnokamisama@gmail.com>
2020-11-04 12:14:26 +0900

    アプリケーションコントローラー記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :f_name, :l_name, :f_name_kana, :l_name_kana, :birthday])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [)]
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   # has_many : buys
 
   validates :nickname, presence: true
-  validates :f_name, presence: true format: {with: /\A[ぁ-んァ-ンー-龥]+\z/}
-  validates :l_name, presence: true format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :f_name, presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/}
+  validates :l_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :f_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :l_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
