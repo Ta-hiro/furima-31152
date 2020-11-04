e24b85e494ade1e7849f0e1d0d9f6feb490c4dbc
Ta-hiro <erosnokamisama@gmail.com>
2020-11-04 11:32:05 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # has_many : items
+  # has_many : buys
+
+  validates :nickname, presence: true
+  validates :f_name, presence: true format: {with: /\A[ぁ-んァ-ンー-龥]+\z/}
+  validates :l_name, presence: true format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :f_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :l_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday, presence: true
+  validates :encrypted_password, presence: true, format: {with: /\A[a-zA-Z0-9]+\z/}
+  validates :email, presence: true, uniqueness: true
+
 end
