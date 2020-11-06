944b00a240a888f9d0e2b1a750f75bd538c59b02
Ta-hiro <erosnokamisama@gmail.com>
2020-11-06 12:35:07 +0900

    RuboCop使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   # has_many : buys
 
   validates :nickname, presence: true
-  validates :f_name, presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/}
-  validates :l_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :f_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :l_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :f_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
+  validates :l_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :f_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :l_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
-  validates :password, format: {with: /\A[a-zA-Z0-9]+\z/}
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :email, presence: true, uniqueness: true
 end
