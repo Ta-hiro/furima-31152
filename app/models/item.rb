7967bc3442c6a534f649059c9e2c9c0048e5ba46
Ta-hiro <erosnokamisama@gmail.com>
2020-11-06 16:23:23 +0900

    activestorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :product_status
-  belongs_to :delivery_burde
-  belongs_to :prefectures
-  belongs_to :shipping_period
-  
+  belongs_to_active_hash :category
+  belongs_to_active_hash :product_status
+  belongs_to_active_hash :delivery_burde
+  belongs_to_active_hash :prefectures
+  belongs_to_active_hash :shipping_period
+
 end
