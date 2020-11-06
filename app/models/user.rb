20a68349921f16009a9d855c26c7b0e29fcd1fa2
Ta-hiro <erosnokamisama@gmail.com>
2020-11-06 14:15:57 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,18 @@ class User < ApplicationRecord
   # has_many : items
   # has_many : buys
 
-  validates :nickname, presence: true
-  validates :f_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
-  validates :l_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :f_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :l_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
-  validates :email, presence: true, uniqueness: true
+
+  full_width = { with: /\A[ぁ-んァ-ンー-龥]+\z/ }
+  full_width_kana = { with: /\A[ァ-ヶー－]+\z/ }
+
+  with_options presence: true do
+    validates :nickname
+    validates :f_name, format: full_width
+    validates :l_name, format: full_width
+    validates :f_name_kana, format: full_width_kana
+    validates :l_name_kana, format: full_width_kana
+    validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+    validates :email, uniqueness: true
+    validates :birthday
+  end
 end
