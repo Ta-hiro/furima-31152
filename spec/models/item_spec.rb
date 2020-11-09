b5fc48033bc68134267bfbb8d154c1f3608842d9
Ta-hiro <erosnokamisama@gmail.com>
2020-11-09 11:13:02 +0900

    ダミーデータ表示分岐、不要記述削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,67 +17,56 @@ RSpec.describe Item, type: :model do
       it 'imageが無い' do
         @item.image = nil
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'product_nameが空' do
         @item.product_name = ''
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include("Product name can't be blank")
       end
       it 'explanationが空' do
         @item.explanation = ''
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
       it 'category_idが0を選択している' do
         @item.category_id = 0
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include('Category must be other than 0')
       end
       it 'product_status_idが0を選択している' do
         @item.product_status_id = 0
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include('Product status must be other than 0')
       end
       it 'delivery_burden_idが0を選択している' do
         @item.delivery_burden_id = 0
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include('Delivery burden must be other than 0')
       end
       it 'prefectures_idが0を選択している' do
         @item.prefectures_id = 0
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include('Prefectures must be other than 0')
       end
       it 'shipping_period_idが0を選択している' do
         @item.shipping_period_id = 0
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include('Shipping period must be other than 0')
       end
       it 'priceが空' do
         @item.price = ''
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが300円未満' do
         @item.price = 299
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが10000000円以上' do
         @item.price = 10_000_000
         @item.valid?
-        # binding.pry
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
     end
